b59b512847083c311cb9e0e3ef74c7c5058e58f3
Tophurkey <dualgatling@gmail.com>
2020-09-20 00:10:52 -0700

    end of 1st day
    skeleton started, still fidgetting with buttons for creating login pages

--- FlaskApp/FlaskApp/__init__.py
+++ FlaskApp/FlaskApp/__init__.py
@@ -6,5 +6,25 @@ app = Flask(__name__)
 def homepage():
 	return render_template("main.html")
 
+@app.route('/register/')
+def register():
+	return render_template("register.html")
+
+@app.route('/scientist/')
+def scientist():
+	return render_template("scientist.html")
+
+@app.route('/teacher/')
+def teacher():
+	return render_template("teacher.html")
+
+@app.route('/dashboard/')
+def dashboard():
+	return render_template("dashboard.html")
+
+@app.route('/header/')
+def header():
+	return render_template("header.html")
+
 if __name__ == "__main__":
 	app.run()
